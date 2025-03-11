88933aa2ca5ae436e64cd46fb4c3436e3b8a1e59
RaphCodes1 <raphaelcreer@gmail.com>
2025-03-12 00:07:32 +0400

    norm on error.c and main.c

--- error.c
+++ error.c
@@ -1,65 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcreer <rcreer@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 18:02:12 by rcreer            #+#    #+#             */
+/*   Updated: 2025/03/06 18:15:04 by rcreer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-void free_textures(t_cub *cub)
+void	free_textures(t_cub *cub)
 {
-    if (cub->north)
-    {
-        if (cub->north->img.img)
-            mlx_destroy_image(cub->connection, cub->north->img.img);
-        free(cub->north);
-    }
-    if (cub->south)
-    {
-        if (cub->south->img.img)
-            mlx_destroy_image(cub->connection, cub->south->img.img);
-        free(cub->south);
-    }
-    if (cub->east)
-    {
-        if (cub->east->img.img)
-            mlx_destroy_image(cub->connection, cub->east->img.img);
-        free(cub->east);
-    }
-    if (cub->west)
-    {
-        if (cub->west->img.img)
-            mlx_destroy_image(cub->connection, cub->west->img.img);
-        free(cub->west);
-    }
+	if (cub->north)
+	{
+		if (cub->north->img.img)
+			mlx_destroy_image(cub->connection, cub->north->img.img);
+		free(cub->north);
+	}
+	if (cub->south)
+	{
+		if (cub->south->img.img)
+			mlx_destroy_image(cub->connection, cub->south->img.img);
+		free(cub->south);
+	}
+	if (cub->east)
+	{
+		if (cub->east->img.img)
+			mlx_destroy_image(cub->connection, cub->east->img.img);
+		free(cub->east);
+	}
+	if (cub->west)
+	{
+		if (cub->west->img.img)
+			mlx_destroy_image(cub->connection, cub->west->img.img);
+		free(cub->west);
+	}
 }
 
-// void free_map(t_cub *cub)
-// {
-//     int i;
-
-//     i = 0;
-//     while (cub->final_map[i])
-//     {
-//         free(cub->final_map[i]);
-//         i++;
-//     }
-//     free(cub->final_map);
-// }
-
-int error_msg(int flag)
+int	error_msg(int flag)
 {
-    if (flag == ERR_MALLOC)
-        write(2, "Malloc failed\n", 15);
-    else if (flag == ERR_FILE)
-        write(2, "File error\n", 12);
-    else if (flag == ERR_SYS_ARGS)
-        write(2, "Arguments error\n", 17);
-    else if (flag == ERR_MLX)
-        write(2, "Mlx function error\n", 20);
-    return (EXIT_FAILURE);
-}   
+	if (flag == ERR_MALLOC)
+		write(2, "Malloc failed\n", 15);
+	else if (flag == ERR_FILE)
+		write(2, "File error\n", 12);
+	else if (flag == ERR_SYS_ARGS)
+		write(2, "Arguments error\n", 17);
+	else if (flag == ERR_MLX)
+		write(2, "Mlx function error\n", 20);
+	return (EXIT_FAILURE);
+}
 
-void cub_slayer(t_cub *cub)
+void	cub_slayer(t_cub *cub)
 {
-    // free_map(cub);
-    free_textures(cub);
-    mlx_destroy_image(cub->connection, cub->img.img);
-    mlx_destroy_window(cub->connection, cub->win);
-    free(cub->connection);
-    free(cub);
+	free_textures(cub);
+	mlx_destroy_image(cub->connection, cub->img.img);
+	mlx_destroy_window(cub->connection, cub->win);
+	free(cub->connection);
+	free(cub);
 }
--- main.c
+++ main.c
@@ -1,84 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcreer <rcreer@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 18:02:12 by rcreer            #+#    #+#             */
+/*   Updated: 2025/03/06 18:15:04 by rcreer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-int rgb_to_int(int *color)
+int	rgb_to_int(int *color)
 {
-    return (color[0] << 16) | (color[1] << 8) | color[2];
+	return ((color[0] << 16) | (color[1] << 8) | color[2]);
 }
 
-void cub_spawner(t_cub *cub)
+void	cub_spawner(t_cub *cub)
 {
-    cub->connection = NULL;
-    cub->img.img = NULL;
-    cub->img.addr = NULL;
-    cub->north = NULL;
-    cub->south = NULL;
-    cub->east = NULL;
-    cub->west = NULL;
+	cub->connection = NULL;
+	cub->img.img = NULL;
+	cub->img.addr = NULL;
+	cub->north = NULL;
+	cub->south = NULL;
+	cub->east = NULL;
+	cub->west = NULL;
 }
 
-void cub_init(t_cub *cub, t_parsing *parse)
+void	cub_init(t_cub *cub, t_parsing *parse)
 {
-    cub_spawner(cub);
-    cub->connection = mlx_init();
-    cub->win = mlx_new_window(cub->connection, screenWidth, screenHeight, "cub3D");
-    cub->img.img = mlx_new_image(cub->connection, screenWidth, screenHeight);
-    cub->img.addr = mlx_get_data_addr(cub->img.img, &cub->img.bits_per_pixel, &cub->img.line_length, &cub->img.endian);
-    cub->direc = parse->player;
-    cub->map = parse->map;
-    init_direction(cub, cub->direc);
-    cub->posX = parse->x_pos + 0.5;
-    cub->posY = parse->y_pos + 0.5;
-    init_textures(cub, parse);
-    cub->floor = rgb_to_int(parse->rgb[0]);
-    cub->ceil = rgb_to_int(parse->rgb[1]);
-    cub->parse = parse;
+	cub_spawner(cub);
+	cub->connection = mlx_init();
+	cub->win = mlx_new_window(cub->connection, screenWidth,
+			screenHeight, "cub3D");
+	cub->img.img = mlx_new_image(cub->connection, screenWidth,
+			screenHeight);
+	cub->img.addr = mlx_get_data_addr(cub->img.img, &cub->img.bits_per_pixel,
+			&cub->img.line_length, &cub->img.endian);
+	cub->direc = parse->player;
+	cub->map = parse->map;
+	init_direction(cub, cub->direc);
+	cub->posX = parse->x_pos + 0.5;
+	cub->posY = parse->y_pos + 0.5;
+	init_textures(cub, parse);
+	cub->floor = rgb_to_int(parse->rgb[0]);
+	cub->ceil = rgb_to_int(parse->rgb[1]);
+	cub->parse = parse;
 }
 
-int cub_rendering(t_cub *cub)
+int	cub_rendering(t_cub *cub)
 {
-    int x = 0;
-    int y = 0;
+	int	x;
+	int	y;
 
-    while (x < screenWidth)
-    {
-        perform_ray_casting(x, cub);
-        perform_dda(cub, cub->parse);
-        if (cub->side == 0) 
-            cub->perpWallDist = (cub->sideDistX - cub->deltaDistX);
-        else          
-            cub->perpWallDist = (cub->sideDistY - cub->deltaDistY);
-        get_drawing_coords(cub);
-        draw_strip(x, cub->drawStart, cub->drawEnd, cub);
-        x++;
-    }
-    mlx_put_image_to_window(cub->connection, cub->win, cub->img.img, 0, 0);
-    return (1);
+	x = 0;
+	y = 0;
+	while (x < screenWidth)
+	{
+		perform_ray_casting(x, cub);
+		perform_dda(cub, cub->parse);
+		if (cub->side == 0)
+			cub->perpWallDist = (cub->sideDistX - cub->deltaDistX);
+		else
+			cub->perpWallDist = (cub->sideDistY - cub->deltaDistY);
+		get_drawing_coords(cub);
+		draw_strip(x, cub->drawStart, cub->drawEnd, cub);
+		x++;
+	}
+	mlx_put_image_to_window(cub->connection, cub->win, cub->img.img, 0, 0);
+	return (1);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    t_cub       *cub;
-    t_parsing   *parse;
+	t_cub		*cub;
+	t_parsing	*parse;
 
-    if(ac == 2)
-    {   
-        if(!valid_cub_file(av[1]))
-            return (0);
-        if(!parse_init_save(&parse, av))
-            return (free_data(parse), 0);
-        cub = (t_cub *)malloc(sizeof(t_cub));
-        if (!cub)
-        {
-            free_data(parse);
-            exit(error_msg(ERR_MALLOC));
-        }
-        cub_init(cub, parse);
-        mlx_hook(cub->win, ON_KEY_UP, MASK_KEY_UP, handle_key_event, cub);
-        mlx_hook(cub->win, ON_DESTROY, MASK_ON_DESTROY, handle_closing, cub);
-        mlx_loop_hook(cub->connection, cub_rendering, cub);
-        mlx_loop(cub->connection);
-        free_data(parse);
-    }
-    else
-        exit(error_msg(ERR_SYS_ARGS));
+	if (ac == 2)
+	{
+		if (!valid_cub_file(av[1]))
+			return (0);
+		if (!parse_init_save(&parse, av))
+			return (free_data(parse), 0);
+		cub = (t_cub *)malloc(sizeof(t_cub));
+		if (!cub)
+		{
+			free_data(parse);
+			exit(error_msg(ERR_MALLOC));
+		}
+		cub_init(cub, parse);
+		mlx_hook(cub->win, ON_KEY_UP, MASK_KEY_UP, handle_key_event, cub);
+		mlx_hook(cub->win, ON_DESTROY, MASK_ON_DESTROY, handle_closing, cub);
+		mlx_loop_hook(cub->connection, cub_rendering, cub);
+		mlx_loop(cub->connection);
+		free_data(parse);
+	}
+	else
+		exit(error_msg(ERR_SYS_ARGS));
 }
